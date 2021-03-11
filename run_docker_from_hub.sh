f75a9ae0cd17341503d46c68f9d21e77de35fb93
Georg Novotny <georg.novotny@chello.at>
2021-03-11 15:26:10 +0100

    Fidxed name error

---
+++ run_docker_from_hub.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+USER_ID="$(id -u)"
+XSOCK=/tmp/.X11-unix
+XAUTH=$HOME/.Xauthority
+SHARED_DOCKER_DIR=/home/fhtw_user/catkin_ws/src/fhtw
+SHARED_HOST_DIR=$(pwd)/src
+VOLUMES="--volume=$XSOCK:$XSOCK:rw
+         --volume=$XAUTH:$XAUTH:rw
+         --volume=$SHARED_HOST_DIR:$SHARED_DOCKER_DIR:rw"
+
+
+mkdir -p "$SHARED_HOST_DIR"
+
+
+echo -e "\e[32mMounting fodler:
+    $SHARED_HOST_DIR    to
+    $SHARED_DOCKER_DIR\e[0m"
+
+
+docker run \
+    -it --rm \
+    $VOLUMES \
+    --env="XAUTHORITY=${XAUTH}" \
+    --env="DISPLAY=${DISPLAY}" \
+    --env="USER_ID=$USER_ID" \
+    --privileged \
+    --net=host \
+    --name="taurob_sim" \
+    georgno/fhtw-tracker-sim:latest-cpu \
+    tmux
+
