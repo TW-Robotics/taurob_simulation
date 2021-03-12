ab51093483a520767f0266138ac2743c017b2286
Georg Novotny <georg.novotny@chello.at>
2021-03-12 20:38:31 +0100

    Added windows start batch file

---
+++ run_docker.bat
@@ -0,0 +1,22 @@
+@echo off
+if "%1"==""(
+    VERSION=%1
+) else(
+        VERSION="cpu"
+)
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo +Make sure you have a X11 server runing                                +
+echo +Make sure that "Native opengl" is disabled for the running X11 server +
+for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
+echo +Network IP: %NetworkIP%                                                 +
+set DISPLAY=%NetworkIP%:0.0
+echo +DISPLAY=%DISPLAY%                                                 +
+set pfad=%CD%\catkin_ws\src\
+echo +Path to catkin_ws=%pfad%
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+docker run -it --rm --network bridge  -p 5000:5000 --mount type=bind,source="%pfad%\",target=/home/fhtw_user/catkin_ws/src/fhtw/ --name "taurob_sim" --privileged -e DISPLAY=%DISPLAY%  "georgno/fhtw-tracker-sim:%VERSION%" "tmux"
+
+cmd /k
